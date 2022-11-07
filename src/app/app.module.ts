13ea5b7d84181c7f6a79145258bff52b24260206
Danil Byslenko <danilbuslenko@gmail.com>
2022-11-07 10:39:28 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TostComponent } from './tost/tost.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TostComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tost/tost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tost',
+  templateUrl: './tost.component.html',
+  styleUrls: ['./tost.component.css']
+})
+export class TostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
