1c5b963d5ed49ed6e3d5f517daa2399df0769a4e
Danil Byslenko <danilbuslenko@gmail.com>
2022-11-07 11:03:13 +0200

    t

--- src/app/tost/tost.component.ts
+++ src/app/tost/tost.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TostComponent implements OnInit {
 
   constructor() { }
-
+  img:string='../../assets/c3a1ca5e-b1c0-456c-b35e-5c9ef075ba7e.jpg'
   ngOnInit(): void {
   }
 
